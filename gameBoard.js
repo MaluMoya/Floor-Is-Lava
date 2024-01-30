c7615f07ee205d2e6f35c19bface17eba5072eb3
Malu Moya <malumoyabravo@gmail.comgit config --listgit config --global user.name MaluMoyagit config --global user.email malumoyabravo@gmail.com>
2024-01-30 13:20:58 +0100

    Creating character step 1

--- gameBoard.js
+++ gameBoard.js
@@ -1,21 +1,12 @@
 console.log ("GameBoard Js is working!")
-
+/*
 class Game {
     constructor (){
         this.enemies = [];
-        this.player = new Player(100, 100);
+        this.player = new Player(0, 0);
         this.gameOver = false;
         this.lives = lives;
         this.updateLives();
     }
-    updateLives() {
-        document.querySelector("#lives").innerText = this.lives;
-        if (this.lives <= 0) {
-            this.gameOver = true;
-        }
 
-    }
-
-
-
-}
+}*/
